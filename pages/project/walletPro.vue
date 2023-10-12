2377a591f5e537ac259a64abcaba1ec55e635be5
culaido2018 <culaido@owlting.com>
2023-10-12 16:33:34 +0800

    wallet pro

--- pages/project/walletPro.vue
+++ pages/project/walletPro.vue
@@ -11,7 +11,28 @@ definePageMeta({
 useHead({
   title: 'Wallet Pro'
 })
+
+    import api from '~/api/api.js';
+    let ret;
+    
+    ret = await api.wallet.list();
+    
+console.log(ret);
+    
+    let assetId = ret.data.asset[0].id;
+    
+console.log('assetId : ' + assetId);
+    
+    ret = await api.wallet.asset(assetId);
+
+console.log(ret);
+
+    ret = await api.wallet.balance(assetId);
+console.log(ret);
+
+
 </script>
+
 <style lang="scss" scoped>
 
 </style>
